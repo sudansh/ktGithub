60a17e48268c5d8277a6e773718784008b301225
Sudhanshu Singh <sudansh@sudanshMac.local>
2018-08-28 01:09:28 +0400

    app class name changed

--- app/src/main/java/com/android/example/github/di/AppInjector.kt
+++ app/src/main/java/com/android/example/github/di/AppInjector.kt
@@ -22,7 +22,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v4.app.FragmentManager
-import com.android.example.github.GithubApp
+import com.android.example.github.App
 import dagger.android.AndroidInjection
 import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.HasSupportFragmentInjector
@@ -31,39 +31,38 @@ import dagger.android.support.HasSupportFragmentInjector
  * Helper class to automatically inject fragments if they implement [Injectable].
  */
 object AppInjector {
-    fun init(githubApp: GithubApp) {
-        DaggerAppComponent.builder().application(githubApp)
-            .build().inject(githubApp)
-        githubApp
-            .registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-                override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                    handleActivity(activity)
-                }
+    fun init(app: App) {
+        DaggerAppComponent.builder().application(app)
+            .build().inject(app)
+        app.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                handleActivity(activity)
+            }
 
-                override fun onActivityStarted(activity: Activity) {
+            override fun onActivityStarted(activity: Activity) {
 
-                }
+            }
 
-                override fun onActivityResumed(activity: Activity) {
+            override fun onActivityResumed(activity: Activity) {
 
-                }
+            }
 
-                override fun onActivityPaused(activity: Activity) {
+            override fun onActivityPaused(activity: Activity) {
 
-                }
+            }
 
-                override fun onActivityStopped(activity: Activity) {
+            override fun onActivityStopped(activity: Activity) {
 
-                }
+            }
 
-                override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {
 
-                }
+            }
 
-                override fun onActivityDestroyed(activity: Activity) {
+            override fun onActivityDestroyed(activity: Activity) {
 
-                }
-            })
+            }
+        })
     }
 
     private fun handleActivity(activity: Activity) {
